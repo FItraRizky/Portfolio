5d8b5e782d023b50bf97e2cce8b6cb5e36dbb6e7
fitrarizky <orian3969@gmail.com>
2025-08-02 23:45:37 +0700

    ga

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -18,7 +18,7 @@ const HeroText = () => {
           animate="visible"
           transition={{ delay: 1 }}
         >
-          Hi I'm Ali
+          Hi I'm Ryan
         </motion.h1>
         <div className="flex flex-col items-start">
           <motion.p
@@ -61,7 +61,7 @@ const HeroText = () => {
           animate="visible"
           transition={{ delay: 1 }}
         >
-          Hi,I'm Ali
+          Hi,I'm Ryan
         </motion.p>
         <div>
           <motion.p
